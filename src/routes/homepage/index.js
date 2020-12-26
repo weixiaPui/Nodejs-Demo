c5591af5ad29df87f53ae38c76fa97ebe3e0670c
weixiaPui <wp15484@my.bristol.ac.uk>
2020-12-26 14:45:09 +0000

    feat(helloWorld): render data Use query parameter for dynamic content

--- src/routes/homepage/index.js
+++ src/routes/homepage/index.js
@@ -8,4 +8,12 @@ module.exports = (app) => {
     app.get('/home', (req, res) => {
         res.status(200).render('home/index.ejs')
     })
+    app.get('/helloWorld', (req, res) => {
+        let inputName= req.query['input-name']
+        res.status(200).render('helloWorld/index.ejs', {
+            data: {
+                name: inputName
+            }
+        })
+    })
 }
